d5b6ba01aaf4f753f8fccccb81aeb7ff2f3d15df
irochi1184 <ken.office.arita@gmail.com>
2024-02-26 22:09:49 +0900

    Create New Project

---
+++ Financial_iPhoneApp/Financial_iPhoneAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  Financial_iPhoneAppApp.swift
+//  Financial_iPhoneApp
+//
+//  Created by 有田健一郎 on 2024/02/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Financial_iPhoneAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
