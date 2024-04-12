74f949c6b061dc00e061aa6a1e8dc5fbfa844f85
irochi1184 <ken.office.arita@gmail.com>
2024-04-13 07:35:54 +0900

    main

--- Financial_iPhoneApp/ContentView.swift
+++ Financial_iPhoneApp/ContentView.swift
@@ -8,44 +8,64 @@
 import SwiftUI
 import SwiftData
 
-struct ContentView: View {
+struct ContentView: View { //アプリ起動時の共有画面
     
     init() {
         UITabBar.appearance().backgroundColor = UIColor(Color(0xfaf0e6, alpha:1.0))
         // TabViewの背景色(薄茶色)
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor(Color(0xfaf0e6, alpha:1.0))
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.black, .font : UIFont.systemFont(ofSize: 30)]
+        appearance.titlePositionAdjustment.vertical = 5
+        appearance.accessibilityFrame = CGRect(x: 20, y: 20, width: 20, height: 100)
+        
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
     }
     
     var body: some View {
         
-        TabView{
-            HomeView() //タブ1番目
-                .tabItem {
-                    Image(systemName: "house")
-                    Text("ホーム")
-                }
-            CalenderView() //タブ2番目
-                .tabItem {
-                    Image(systemName: "calendar")
-                    Text("カレンダー")
-                }
-            GraphView() //タブ3番目
-                .tabItem {
-                    Image(systemName: "chart.line.uptrend.xyaxis")
-                    Text("レポート")
-                }
-            SearchView() //タブ4番目
-                .tabItem {
-                    Image(systemName: "magnifyingglass")
-                    Text("検索")
-                }
-            SettingView() //タブ5番目
-                .tabItem {
-                    Image(systemName: "gearshape")
-                    Text("設定")
-                }
+        // プラスボタン実装
+        NavigationStack {
+            TabView {
+                HomeView() //タブ1番目
+                    .tabItem { //見た目
+                        Image(systemName: "house")
+                        Text("ホーム")
+                    }
+                CalenderView() //タブ2番目
+                    .tabItem {
+                        Image(systemName: "calendar")
+                        Text("カレンダー")
+                    }
+                GraphView() //タブ3番目
+                    .tabItem {
+                        Image(systemName: "chart.line.uptrend.xyaxis")
+                        Text("レポート")
+                    }
+                SearchView() //タブ4番目
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                        Text("検索")
+                    }
+                SettingView() //タブ5番目
+                    .tabItem {
+                        Image(systemName: "gearshape")
+                        Text("設定")
+                    }
+            }
+            .accentColor(.green) //ここでタブのアクセント色の指定
+            
+            //タイトル
+            .navigationTitle("\nホーム")
+            .navigationBarTitleDisplayMode(.inline)
+//            .padding(.vertical, 20)
         }
-        .accentColor(.blue) //ここでタブのアクセント色の指定
         
+       
     }
 }
 
--- Financial_iPhoneApp/HomeView.swift
+++ Financial_iPhoneApp/HomeView.swift
@@ -8,12 +8,20 @@
 import SwiftUI
 import Charts
 
+
+
 struct FavoriteFruit {
     let name: String
     let count: Int
 }
 
 struct HomeView: View {
+    
+    init() {
+        dateFormatter.dateFormat = "YYYY年MM月"
+        dateFormatter.locale = Locale(identifier: "ja_jp")
+    }
+    
     @State private var favoriteFruits: [FavoriteFruit] = [
         .init(name: "りんご", count: 15),
         .init(name: "いちご", count: 12),
@@ -22,9 +30,46 @@ struct HomeView: View {
         .init(name: "バナナ", count: 4),
         .init(name: "オレンジ", count: 2)
     ]
+    @State var showDatePicker = false
+    @State var date = Date()
+    @State var dateText = ""
+    private let dateFormatter = DateFormatter()
+    
     
     var body: some View {
         VStack {
+            //            DatePicker.init("", selection: $date, displayedComponents: [.date])
+            //                .labelsHidden()
+            //                .datePickerStyle(.wheel)
+            VStack {
+                Button {
+                    showDatePicker.toggle()
+                } label: {
+                    // Text(dateText)
+                    Text(dateText.isEmpty ? "\(dateFormatter.string(from: date))" : dateText)
+                        .onAppear {
+                            Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                                self.date = Date()
+                                dateText = "\(dateFormatter.string(from: date))"
+                            }
+                        }
+                }
+                
+                
+            }
+            
+            if showDatePicker {
+                Rectangle()
+                    .fill(.ultraThinMaterial)
+                    .ignoresSafeArea()
+                    .onTapGesture {
+                        showDatePicker = false
+                    }
+                DatePicker.init("", selection: $date, displayedComponents: .date)
+                    .datePickerStyle(.wheel) // カレンダーを表示
+                    .environment(\.locale, Locale(identifier: "ja_JP"))
+            }
+            
             Chart(favoriteFruits, id: \.name) { favoriteFruit in
                 SectorMark(
                     angle: .value("count", favoriteFruit.count),innerRadius: .inset(30)
@@ -36,6 +81,7 @@ struct HomeView: View {
     }
     
 }
+
 
 #Preview {
     HomeView()
