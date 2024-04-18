3663ee3f649caba896853ca91f2e15e9a818964b
irochi1184 <ken.office.arita@gmail.com>
2024-04-19 08:30:10 +0900

    update
    タブの切り替えをした際に、タイトル部分がタブ名に応じて変更するように機能修正

--- Financial_iPhoneApp/ContentView.swift
+++ Financial_iPhoneApp/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftData
 
 struct ContentView: View { //アプリ起動時の共有画面
     
+    @State private var selectedTab: Tab = .home // 初期値をホームに設定
+    
     init() {
         UITabBar.appearance().backgroundColor = UIColor(Color(0xfaf0e6, alpha:1.0))
         // TabViewの背景色(薄茶色)
@@ -26,46 +28,72 @@ struct ContentView: View { //アプリ起動時の共有画面
         UINavigationBar.appearance().compactAppearance = appearance
     }
     
+    enum Tab {
+        case home, calendar, report, search, settings
+    }
+    
     var body: some View {
         
         // プラスボタン実装
         NavigationStack {
-            TabView {
+            TabView(selection: $selectedTab) {
                 HomeView() //タブ1番目
                     .tabItem { //見た目
                         Image(systemName: "house")
                         Text("ホーム")
                     }
+                    .tag(Tab.home) // タグを設定
+                
                 CalendarView() //タブ2番目
                     .tabItem {
                         Image(systemName: "calendar")
                         Text("カレンダー")
                     }
+                    .tag(Tab.calendar) // タグを設定
+                
                 GraphView() //タブ3番目
                     .tabItem {
                         Image(systemName: "chart.line.uptrend.xyaxis")
                         Text("レポート")
                     }
+                    .tag(Tab.report) // タグを設定
+                
                 SearchView() //タブ4番目
                     .tabItem {
                         Image(systemName: "magnifyingglass")
                         Text("検索")
                     }
+                    .tag(Tab.search) // タグを設定
+                
                 SettingView() //タブ5番目
                     .tabItem {
                         Image(systemName: "gearshape")
                         Text("設定")
                     }
+                    .tag(Tab.settings) // タグを設定
             }
             .accentColor(.green) //ここでタブのアクセント色の指定
             
             //タイトル
-            .navigationTitle("\nホーム")
+            .navigationTitle(title(for: selectedTab))
             .navigationBarTitleDisplayMode(.inline)
-//            .padding(.vertical, 20)
         }
-        
-       
+    }
+    
+    // タブに応じたタイトルを返す関数
+    private func title(for tab: Tab) -> String {
+        switch tab {
+        case .home:
+            return "ホーム"
+        case .calendar:
+            return "カレンダー"
+        case .report:
+            return "レポート"
+        case .search:
+            return "検索"
+        case .settings:
+            return "設定"
+        }
     }
 }
 
