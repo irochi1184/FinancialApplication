fd8fabcb2a7339dc384a43add67ba4a292398d59
irochi1184 <ken.office.arita@gmail.com>
2024-07-07 15:27:38 +0900

    refactoring: エラー潰し、タブ選択の背景色が変更されてしまう現象に対応

--- Financial_iPhoneApp/Financial_iPhoneAppApp.swift
+++ Financial_iPhoneApp/Financial_iPhoneAppApp.swift
@@ -10,10 +10,35 @@ import SwiftData
 
 @main
 struct Financial_iPhoneAppApp: App {
+    @StateObject private var model = AppModel()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .modelContainer(for: TransactionData.self) // データ保存用
+            if model.isReady {
+                ContentView()
+                    .environment(\.modelContext, model.container!.mainContext) // データ保存用
+            } else {
+                ProgressView("Loading...") // ロード中の表示
+                    .onAppear {
+                        model.load()
+                    }
+            }
         }
     }
 }
+
+class AppModel: ObservableObject {
+    @Published var isReady = false
+    var container: ModelContainer?
+    
+    func load() {
+        do {
+            container = try ModelContainer(for: TransactionData.self)
+            self.isReady = true
+        } catch {
+            print("Failed to initialize ModelContainer: \(error)")
+            // 必要に応じてエラーハンドリングを追加
+        }
+    }
+}
+
