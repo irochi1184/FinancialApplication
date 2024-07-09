d964a89674b8e1bfc0aa42348dd9d01db4aa8cce
irochi1184 <ken.office.arita@gmail.com>
2024-07-09 22:18:25 +0900

    Update GraphView.swift

--- Financial_iPhoneApp/MainView/GraphView.swift
+++ Financial_iPhoneApp/MainView/GraphView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import SwiftData
 import Charts
 
 struct LineData: Identifiable {
@@ -18,39 +17,52 @@ struct LineData: Identifiable {
 
 struct GraphView: View {
     
-    @Query private var datas: [TransactionData] // トランザクションデータの取得
-    
     let calendar = Calendar.current
     let formatter = DateFormatter()
-    let formatter2 = DateFormatter()
     
     @State private var selectedDate: Date = Date()
     @State private var selectedDateString: String = ""
-    @State private var selectedYear: Int = Calendar.current.component(.year, from: Date())
+    @State private var selectedDay: Date?
+    
+    init() {
+        formatter.dateFormat = "yyyy年"
+    }
+    
     @State private var isDatePickerVisible = false
+    
+    // 選択された年
+    @State private var selectedYear: Int = Calendar.current.component(.year, from: Date())
     
     // 表示する年の範囲
     private let minYear: Int = 2000
     private let maxYear: Int = 2024
     
-    @State private var isOn: Bool = true
-    @State private var isOn1: Bool = true
-    @State private var isOn2: Bool = true
+    let lineData_test: [LineData] = [
+        .init(month: "1月", amount: 1000, category: "all"),
+        .init(month: "2月", amount: 2000, category: "all"),
+        .init(month: "3月", amount: 3000, category: "all"),
+        .init(month: "4月", amount: 4000, category: "all"),
+        .init(month: "5月", amount: 5000, category: "all"),
+        .init(month: "6月", amount: 6000, category: "all"),
+        .init(month: "7月", amount: 1000, category: "all"),
+        .init(month: "8月", amount: 2000, category: "all"),
+        .init(month: "9月", amount: 3000, category: "all"),
+        .init(month: "10月", amount: 4000, category: "all"),
+        .init(month: "11月", amount: 5000, category: "all"),
+        .init(month: "12月", amount: 6000, category: "all"),
+    ]
     
-    init() {
-        formatter.dateFormat = "yyyy年"
-        formatter2.dateFormat = "M月"
-        formatter2.locale = Locale(identifier: "ja_JP")
-    }
+    @State  var isOn:Bool = true
+    @State  var isOn1:Bool = true
+    @State  var isOn2:Bool = true
     
     var body: some View {
-        VStack {
+        VStack{
             VStack {
-                // 年の切り替えボタン
+                // 月の切り替えボタン
                 HStack {
                     Button(action: {
-                        self.selectedDate = self.calendar.date(byAdding: .year, value: -1, to: self.selectedDate)!
-                        self.selectedYear = Calendar.current.component(.year, from: self.selectedDate)
+                        self.selectedDate = self.calendar.date(byAdding: .month, value: -1, to: self.selectedDate)!
                     }) {
                         Image(systemName: "chevron.left")
                     }
@@ -70,8 +82,7 @@ struct GraphView: View {
                     Spacer()
                     
                     Button(action: {
-                        self.selectedDate = self.calendar.date(byAdding: .year, value: 1, to: self.selectedDate)!
-                        self.selectedYear = Calendar.current.component(.year, from: self.selectedDate)
+                        self.selectedDate = self.calendar.date(byAdding: .month, value: 1, to: self.selectedDate)!
                     }) {
                         Image(systemName: "chevron.right")
                     }
@@ -80,12 +91,13 @@ struct GraphView: View {
                 
             }
             .padding(.bottom, 20) // 下部に余白を追加
-            .sheet(isPresented: $isDatePickerVisible) { // 年のピッカーを表示するためのシート
+            .sheet(isPresented: $isDatePickerVisible) {
+                // 年月のピッカーを表示するためのシート
                 VStack {
                     // DatePickerを閉じるボタン
                     Button(action: {
                         self.isDatePickerVisible = false
-                        self.selectedDate = self.calendar.date(from: DateComponents(year: selectedYear)) ?? Date() // 確定ボタンが押されたときの処理
+
                     }) {
                         Text("閉じる")
                             .foregroundColor(.blue)
@@ -107,7 +119,7 @@ struct GraphView: View {
                 }.presentationDetents([.height(280)]) // シートの高さ
             }
             VStack {
-                Chart(calculateMonthlyUsageAmount()){ dataRow in
+                Chart(lineData_test){ dataRow in
                     LineMark(
                         x: .value("month", dataRow.month),
                         y: .value("amount", dataRow.amount)
@@ -118,8 +130,8 @@ struct GraphView: View {
                 .chartYAxis{
                     AxisMarks(position: .leading)
                 }
-                List {
-                    Toggle(isOn: $isOn) {
+                List{
+                    Toggle(isOn: $isOn){
                         Text("全体")
                     }
                     Toggle(isOn: $isOn1) {
@@ -131,26 +143,6 @@ struct GraphView: View {
                 }
             }
         }
-        .onAppear {
-            selectedDateString = formatter.string(from: selectedDate)
-        }
-    }
-    
-    // 各月の使用金額を計算する関数
-    private func calculateMonthlyUsageAmount() -> [LineData] {
-        var monthlyUsage: [LineData] = []
-        for month in 1...12 {
-            let monthData = datas.filter { data in
-                let components = calendar.dateComponents([.year, .month], from: data.selectedDate)
-                return components.year == selectedYear && components.month == month
-            }
-            let totalAmount = monthData.reduce(0) { $0 + (Int($1.amount) ?? 0) }
-            let dateComponents = DateComponents(year: selectedYear, month: month)
-            let monthDate = calendar.date(from: dateComponents)!
-            let monthString = formatter2.string(from: monthDate)
-            monthlyUsage.append(LineData(month: monthString, amount: totalAmount, category: "all"))
-        }
-        return monthlyUsage
     }
 }
 
